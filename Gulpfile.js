16e1c91bbe019ec05f4de15576854ddcd7f97516
DESKTOP-EDV6BLH\oskarinmix <oscaralbertoceballos@gmail.com>
2017-05-20 11:59:17 -0400

    D

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
 gulp.task('compilar_sass', function() {
   gulp
-    .src('sass/index.sass')
+    .src('curso-git/sass/index.sass')
     .pipe(sass())
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('curso-git/css'));
 });
-
-// watch Sass files for changes, run the Sass preprocessor with the 'sass' task and reload
-  gulp.watch('sass/*.sass', ['compilar_sass']);
-  
+// gulp.task('reload', ['compilar_sass'], function() {
+//   browserSync({
+//     server: {
+//       baseDir: 'curso-git'
+//     }
+//   });
+// });
+gulp.watch('curso-git/sass/*.sass', ['compilar_sass']);  
+//gulp.watch(['curso-git/*.html', 'curso-git/css/*.css', 'curso-git/js/*.js','curso-git/sass/*.sass'], {cwd: 'curso-git'}, reload);
 gulp.task('default', ['compilar_sass']);
